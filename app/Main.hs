01be505dbd7051ba6b6abc10dfbd86f068549387
David3141 <13077061+David3141@users.noreply.github.com>
2024-12-21 23:58:05 +0100

    Change Main to automatically pass input into each part

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,8 @@ import System.Environment (getArgs)
 import System.TimeIt (timeIt)
 
 import qualified Day01
+import Text.Printf (printf)
+import Paths_aoc2024 ( getDataFileName )
 
 main :: IO ()
 main = do
@@ -14,11 +16,17 @@ run 1 = runPretty 1 (Day01.part1, Day01.part2)
 run _ = putStrLn "Not implemented"
 
 
-runPretty :: (Show a, Show b) => Int -> (IO a, IO b) -> IO ()
+runPretty :: (Show a, Show b) => Int -> (String -> a, String -> b) -> IO ()
 runPretty day (part1, part2) = do
+  let inputFileName = "inputs/day" ++ printf "%02d" day ++ ".txt"
+  input <- readFile =<< getDataFileName inputFileName
+
   putStrLn $ "--- Day " ++ show day ++ " ---"
+
   putStr "Part 1:     "
-  timeIt (print =<< part1)
+  timeIt (print $ part1 input)
+
   putStr "\nPart 2:     "
-  timeIt (print =<< part2)
+  timeIt (print $ part2 input)
+
   putStrLn ""
--- src/Day01.hs
+++ src/Day01.hs
@@ -1,35 +1,36 @@
-module Day01 (part1, part2) where
+module Day01 (part1, part2, prepare) where
 
-import Data.List (sort, transpose, nub)
-import Helpers (readStrings)
+import Data.List (nub, sort, transpose)
 
-part1 :: IO Int
-part1 = sum . map abs . uncurry (zipWith (-)) <$> readLists
+part1 :: String -> Int
+part1 = sum . map abs . uncurry (zipWith (-)) . prepare
 
-part2 :: IO Int
-part2 = do
-  (listA, listB) <- readLists
+part2 :: String -> Int
+part2 input = sum . map countInBothAndMultiply $ nub listA
+  where
+    (listA, listB) = prepare input
 
-  let countInBothAndMultiply x = x * countElemInSortedList x listA * countElemInSortedList x listB
+    countInBothAndMultiply :: Int -> Int
+    countInBothAndMultiply x = x * countElemInSortedList x listA * countElemInSortedList x listB
 
-  return . sum . map countInBothAndMultiply $ nub listA
-
-countElemInSortedList :: Ord a => a -> [a] -> Int
+countElemInSortedList :: (Ord a) => a -> [a] -> Int
 countElemInSortedList element list = count' element list 0
   where
-    count' :: Ord a => a -> [a] -> Int -> Int
+    count' :: (Ord a) => a -> [a] -> Int -> Int
     count' _ [] currentResult = currentResult
-    count' x (y:ys) currentResult
+    count' x (y : ys) currentResult
       | x > y = count' x ys currentResult
       | x == y = count' x ys (currentResult + 1)
       | otherwise = currentResult
 
-readLists :: IO ([Int], [Int])
-readLists = do
-  [listA, listB] <-
-    map (sort . map read)
-      . transpose
-      . map words
-      <$> readStrings "inputs/day01.txt"
-
-  return (listA, listB)
+prepare :: String -> ([Int], [Int])
+prepare input = case processed of
+  [listA, listB] -> (listA, listB)
+  _ -> error "Unexpected input format"
+  where
+    processed =
+      map (sort . map read)
+        . transpose
+        . map words
+        . lines
+        $ input
