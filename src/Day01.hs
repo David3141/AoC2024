19c1ffbe8f0ec79a20412cf041009a73cd55bb1e
David3141 <13077061+David3141@users.noreply.github.com>
2024-12-21 22:42:52 +0100

    Add day 1 part 2

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,13 +1,28 @@
 module Day01 (part1, part2) where
 
-import Data.List (sort, transpose)
+import Data.List (sort, transpose, nub)
 import Helpers (readStrings)
 
 part1 :: IO Int
 part1 = sum . map abs . uncurry (zipWith (-)) <$> readLists
 
 part2 :: IO Int
-part2 = return 2000
+part2 = do
+  (listA, listB) <- readLists
+
+  let countInBothAndMultiply x = x * countElemInSortedList x listA * countElemInSortedList x listB
+
+  return . sum . map countInBothAndMultiply $ nub listA
+
+countElemInSortedList :: Ord a => a -> [a] -> Int
+countElemInSortedList element list = count' element list 0
+  where
+    count' :: Ord a => a -> [a] -> Int -> Int
+    count' _ [] currentResult = currentResult
+    count' x (y:ys) currentResult
+      | x > y = count' x ys currentResult
+      | x == y = count' x ys (currentResult + 1)
+      | otherwise = currentResult
 
 readLists :: IO ([Int], [Int])
 readLists = do
@@ -18,4 +33,3 @@ readLists = do
       <$> readStrings "inputs/day01.txt"
 
   return (listA, listB)
-  
