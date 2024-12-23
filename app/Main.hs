2a34beaf516713d2c63e6ad230bfee99ed905973
David3141 <13077061+David3141@users.noreply.github.com>
2024-12-23 19:31:17 +0100

    Add templates for all remaining days 5..23

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,25 @@ module Main where
 import System.Environment (getArgs)
 import System.TimeIt (timeIt)
 
+import qualified Day23
+import qualified Day22
+import qualified Day21
+import qualified Day20
+import qualified Day19
+import qualified Day18
+import qualified Day17
+import qualified Day16
+import qualified Day15
+import qualified Day14
+import qualified Day13
+import qualified Day12
+import qualified Day11
+import qualified Day10
+import qualified Day09
+import qualified Day08
+import qualified Day07
+import qualified Day06
+import qualified Day05
 import qualified Day04
 import qualified Day03
 import qualified Day02
@@ -19,6 +38,25 @@ run 1 = runPretty 1 (Day01.part1, Day01.part2)
 run 2 = runPretty 2 (Day02.part1, Day02.part2)
 run 3 = runPretty 3 (Day03.part1, Day03.part2)
 run 4 = runPretty 4 (Day04.part1, Day04.part2)
+run 5 = runPretty 5 (Day05.part1, Day05.part2)
+run 6 = runPretty 6 (Day06.part1, Day06.part2)
+run 7 = runPretty 7 (Day07.part1, Day07.part2)
+run 8 = runPretty 8 (Day08.part1, Day08.part2)
+run 9 = runPretty 9 (Day09.part1, Day09.part2)
+run 10 = runPretty 10 (Day10.part1, Day10.part2)
+run 11 = runPretty 11 (Day11.part1, Day11.part2)
+run 12 = runPretty 12 (Day12.part1, Day12.part2)
+run 13 = runPretty 13 (Day13.part1, Day13.part2)
+run 14 = runPretty 14 (Day14.part1, Day14.part2)
+run 15 = runPretty 15 (Day15.part1, Day15.part2)
+run 16 = runPretty 16 (Day16.part1, Day16.part2)
+run 17 = runPretty 17 (Day17.part1, Day17.part2)
+run 18 = runPretty 18 (Day18.part1, Day18.part2)
+run 19 = runPretty 19 (Day19.part1, Day19.part2)
+run 20 = runPretty 20 (Day20.part1, Day20.part2)
+run 21 = runPretty 21 (Day21.part1, Day21.part2)
+run 22 = runPretty 22 (Day22.part1, Day22.part2)
+run 23 = runPretty 23 (Day23.part1, Day23.part2)
 run _ = putStrLn "Not implemented"
 
 
---
+++ src/Day16.hs
@@ -0,0 +1,10 @@
+module Day16 (part1, part2) where
+
+part1 :: String -> Int
+part1 = const 1 . prepare
+
+part2 :: String -> Int
+part2 = const 2 . prepare
+
+prepare :: String -> String
+prepare = id
