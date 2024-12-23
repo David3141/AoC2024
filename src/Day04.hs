e47388a248fee5fed908c08621beed567bd86ef2
David3141 <13077061+David3141@users.noreply.github.com>
2024-12-23 19:25:23 +0100

    Day 04 part 2

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,7 +1,13 @@
 module Day04 (part1, part2) where
 
-import MatrixHelpers (Matrix, allDirectionsOfLength, fromStrings)
-import Data.Array (Ix(range), bounds)
+import Data.Array (Ix (range), bounds)
+import MatrixHelpers
+  ( Matrix,
+    allDirectionsOfLength,
+    fromStrings,
+    neighbors,
+    (!),
+  )
 
 part1 :: String -> Int
 part1 input = length . filter (== "XMAS") $ allWords
@@ -11,10 +17,24 @@ part1 input = length . filter (== "XMAS") $ allWords
     allWords = concatMap (\coords -> allDirectionsOfLength 4 coords matrix) allIndices
 
 part2 :: String -> Int
-part2 = const 2 . prepare
+part2 input = length allXMas
+  where
+    matrix = prepare input
+    allIndices = range . bounds $ matrix
+    allXMas = filter isXMas allIndices
+
+    isXMas :: (Int, Int) -> Bool
+    isXMas (m, n)
+      | m == 1 = False
+      | n == 1 = False
+      | element == 'A' = checkCorners
+      | otherwise = False
+      where
+        element = matrix ! (m, n)
+        checkCorners = corners == "MSMS" || corners == "MMSS" || corners == "SMSM" || corners == "SSMM"
+        corners = case neighbors (m, n) matrix of
+          [topLeft, _, topRight, _, _, bottomLeft, _, bottomRight] -> [topLeft, topRight, bottomLeft, bottomRight]
+          _ -> []
 
 prepare :: String -> Matrix Char
 prepare = fromStrings id . lines
-
--- input :: IO String
--- input = readFile =<< getDataFileName "inputs/day04.txt"
